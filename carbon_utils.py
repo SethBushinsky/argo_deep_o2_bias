40c5e3992ae92abe3d5c302eba4aca97de960f7a
Veronica Tamsitt <veronica.tamsitt@gmail.com>
2023-01-09 13:10:35 -0800

    fixed argo_interp_temp.nc wmo check indexing, converted some repeat use code to functions in carbon_utils

--- carbon_utils.py
+++ carbon_utils.py
@@ -6,9 +6,9 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.14.0
+#       jupytext_version: 1.11.3
 #   kernelspec:
-#     display_name: Python 3 (ipykernel)
+#     display_name: Python 3
 #     language: python
 #     name: python3
 # ---
@@ -74,3 +74,61 @@ def LIAR_matlab(LIAR_path,Coordinates,Measurements, MeasIDVec,VerboseTF=False):
     results = np.asarray(results)
 
     return results
+
+
+def sigma0(salinity,temperature,lon,lat,pressure):
+    SA = gsw.SA_from_SP(salinity,
+                        pressure,
+                        lon,
+                        lat)
+
+    CT = gsw.CT_from_t(SA,
+                       temperature,
+                       pressure)
+
+    sigma = gsw.sigma0(SA,CT)
+    
+    return sigma
+
+
+def spiciness0(salinity,temperature,lon,lat,pressure):
+    SA = gsw.SA_from_SP(salinity,
+                        pressure,
+                        lon,
+                        lat)
+
+    CT = gsw.CT_from_t(SA,
+                       temperature,
+                       pressure)
+
+    spiciness = gsw.spiciness0(SA,CT)
+    
+    return spiciness
+
+
+def co2sys_pH25C(pH,temperature,salinity,pressure):
+    #   *SOCCOM* version modified by Nancy Williams on 10/15/15 according to
+    #    Dickson in 9/7/15 e-mail and in Dickson et al. 2007 
+    #    changed KF to Perez and Fraga 1987
+    #    Last three inputs should be ... 1,10,3)
+    results = pyco2.sys(
+        par1=2300., 
+        par2=pH,
+        par1_type=1,
+        par2_type=3,
+        temperature=temperature, 
+        pressure=pressure, 
+        salinity=salinity, 
+        temperature_out=25., #fixed 25C temperature
+        pressure_out=pressure,
+        opt_pH_scale = 1, #total
+        opt_k_carbonic=10, #Lueker et al. 2000
+        opt_k_bisulfate=1, # Dickson 1990 (Note, matlab co2sys combines KSO4 with TB. option 3 = KSO4 of Dickson & TB of Lee 2010)
+        opt_total_borate=2, # Lee et al. 2010
+        opt_k_fluoride=2, # Perez and Fraga 1987
+        opt_buffers_mode=1, # used to be "buffers_mode='auto'" but seems to have changed in versions of pyco2?
+    )
+
+    pH_25C = results['pH_total_out']
+    
+    return pH_25C
