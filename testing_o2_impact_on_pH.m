ea51db2421abc0ee5eaeea834ec60099f13a574d
Seth Bushinsky <smb4@hawaii.edu>
2023-06-20 11:46:16 -1000

    getting up to date

--- testing_o2_impact_on_pH.m
+++ testing_o2_impact_on_pH.m
@@ -5,15 +5,20 @@ o2_mag = 300; %50:50:300;
 no3_offset = -0.25:.25:0.25;
 no3_mag = 5;
 pH_out = NaN(length(o2_offset), length(o2_mag));
+TA_out = NaN(length(o2_offset), length(o2_mag));
+
 MeasIDVec = [1, 7, 3, 6];
 
-for m=1:length(o2_mag)
+for m=1:length(no3_offset)
     for o = 1:length(o2_offset)
+        
         Coordinates_all = [-149.968     ,  -52.543     , 1500.        ];
-        Measurements_all = [34.50930023,   2.80669999,      no3_mag,   o2_mag(m) - o2_offset(o)];
+        Measurements_all = [34.50930023,   2.80669999,      no3_mag - no3_offset(m),   o2_mag(1) - o2_offset(o)];
 %         Measurements_offset = [ 34.50930023,   2.80669999,           172.22309662];
 
         pH_out(o,m) = LIPHR(Coordinates_all, Measurements_all, MeasIDVec, 'OAAdjustTF', 0, 'verboseTF', 0);
+        TA_out(o,m) = LIAR(Coordinates_all, Measurements_all, MeasIDVec, 'verboseTF', 1);
+
     end
 end
 
@@ -23,11 +28,21 @@ MeasIDVec = [1, 7, 3, 6];
 Coordinates_all = [-149.968     ,  -52.543     , 1500.        ];
 Measurements_all = [34.50930023,   2.80669999,      4,   300];
 orig_pH = LIPHR(Coordinates_all, Measurements_all, MeasIDVec, 'OAAdjustTF', 0, 'verboseTF', 0);
-
+orig_TALK = LIAR(Coordinates_all, Measurements_all, MeasIDVec, 'verboseTF', 0);
+disp('PH, Alk')
+disp(orig_pH)
+disp(orig_TALK)
 Measurements_all = [34.50930023,   2.80669999,      4,   300-5];
 
+disp('Change oxygen by -5')
 pH_O2 = LIPHR(Coordinates_all, Measurements_all, MeasIDVec, 'OAAdjustTF', 0, 'verboseTF', 0);
+disp('pH change')
 pH_O2 - orig_pH
+
+TA_O2 = LIAR(Coordinates_all, Measurements_all, MeasIDVec, 'verboseTF', 0);
+
+disp('TA change')
+TA_O2 - orig_TALK
 
 Measurements_all = [34.50930023,   2.80669999,      4+5*16/154,   300];
 
@@ -102,17 +117,30 @@ for i = 1:model_time-1
     pH_Revelle(i) = pH_change_1_umol_DIC - pH(i);
 end
 %%
-
+i=1
 [DATA,~,~]= CO2SYSSOCCOM_smb(TA(i), DIC(i) , ...
         1,2,SAL, Theta, ...
         Theta ,...
         Press_in,Press_in,SI, PO4,1,10,3);
 DATA_orig = DATA;
 
-[DATA,~,~]= CO2SYSSOCCOM_smb(TA(i)-.15, DIC(i)+1 , ...
+[DATA,~,~]= CO2SYSSOCCOM_smb(TA(i)-1, DIC(i) , ...
         1,2,SAL, Theta, ...
         Theta ,...
         Press_in,Press_in,SI, PO4,1,10,3);
+
+DATA - DATA_orig
+%%
+[DATA_orig,~,~]= CO2SYSSOCCOM_smb(2337.788, 8.085734 , ...
+        1,3,SAL, Theta, ...
+        Theta ,...
+        Press_in,Press_in,SI, PO4,1,10,3);
+
+[DATA,~,~]= CO2SYSSOCCOM_smb(2337.788-.8, 8.085734 , ...
+        1,3,SAL, Theta, ...
+        Theta ,...
+        Press_in,Press_in,SI, PO4,1,10,3);
+
 
 DATA - DATA_orig
 
@@ -152,3 +180,40 @@ ylabel('\Delta mpH / \Delta DIC')
 xlabel('\mumol O2 respired')
 
 print(gcf, '-dpng', '-r800',  [plot_dir  'Respiration_experiment.png'])
+
+%% plots checking pCO2 calculations in derived_3 files
+
+clf; subplot(1,3,1); pcolor(TALK_LIAR); shading flat; colorbar; subplot(1,3,2); pcolor(TALK_w_NO3_ADJUST); ; shading flat; colorbar; 
+subplot(1,3,3); pcolor(TALK_w_NO3_ADJUST - TALK_LIAR); shading flat; colorbar
+
+
+%%
+set(gcf, 'colormap', flipud(brewermap(20, 'Spectral')))
+NO3_ADJUSTED = ncread('1902304_derived_3.nc', 'NITRATE_ADJUSTED');
+NITRATE_ADJUSTED_w_O2_ADJUST = ncread('1902304_derived_3.nc', 'NITRATE_ADJUSTED_w_O2_ADJUST');
+
+TALK_LIAR = ncread('1902304_derived_3.nc', 'TALK_LIAR');
+TALK_LIAR_recalc = ncread('1902304_derived_3.nc', 'TALK_LIAR_recalc');
+
+TALK_w_NO3_ADJUST = ncread('1902304_derived_3.nc', 'TALK_w_NO3_ADJUST');
+
+DOXY_ADJUSTED = ncread('1902304_derived_3.nc', 'DOXY_ADJUSTED');
+%%
+clf; 
+
+subplot(4,3,1); pcolor(juld_grid, press, DOXY_ADJUSTED); shading flat; colorbar; set(gca, 'ydir', 'reverse')
+subplot(4,3,2); pcolor(juld_grid, press, DOXY_ADJUSTED);  shading flat; colorbar; set(gca, 'ydir', 'reverse')
+subplot(4,3,3); pcolor(juld_grid, press, DOXY_ADJUSTED - DOXY_ADJUSTED); shading flat; colorbar; set(gca, 'ydir', 'reverse')
+
+subplot(4,3,4); pcolor(juld_grid, press, NO3_ADJUSTED); shading flat; colorbar; set(gca, 'ydir', 'reverse')
+subplot(4,3,5); pcolor(juld_grid, press, NITRATE_ADJUSTED_w_O2_ADJUST);  shading flat; colorbar; set(gca, 'ydir', 'reverse')
+subplot(4,3,6); pcolor(juld_grid, press, NITRATE_ADJUSTED_w_O2_ADJUST - NO3_ADJUSTED); shading flat; colorbar; set(gca, 'ydir', 'reverse')
+
+subplot(4,3,7); pcolor(juld_grid, press, TALK_LIAR); shading flat; colorbar; set(gca, 'ydir', 'reverse')
+subplot(4,3,8); pcolor(juld_grid, press, TALK_LIAR_recalc);  shading flat; colorbar; set(gca, 'ydir', 'reverse')
+subplot(4,3,9); pcolor(juld_grid, press, TALK_LIAR_recalc - TALK_LIAR); shading flat; colorbar; set(gca, 'ydir', 'reverse'); caxis([-2 2])
+
+subplot(4,3,10); pcolor(juld_grid, press, pCO2_pH_orig_TALK_orig); shading flat; colorbar; set(gca, 'ydir', 'reverse')
+subplot(4,3,11); pcolor(juld_grid, press, pCO2_pH_orig_TALK_no3);  shading flat; colorbar; set(gca, 'ydir', 'reverse')
+subplot(4,3,12); pcolor(juld_grid, press, pCO2_pH_orig_TALK_no3 - pCO2_pH_orig_TALK_orig); shading flat; colorbar; set(gca, 'ydir', 'reverse'); %caxis([-2 2])
+
