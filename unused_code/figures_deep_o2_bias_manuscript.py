58c7a647bdab608941c754c27099ff29bbdf9151
Seth Bushinsky <smb4@hawaii.edu>
2023-11-28 09:51:52 -1000

    making an unused code folder

---
+++ unused_code/figures_deep_o2_bias_manuscript.py
@@ -0,0 +1,492 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.14.0
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# Plotting script for deep O2 bias manuscript
+#
+# 2022_07_21 Seth Bushinsky - will try to piggy-back off code being writting by Veronica Tamsitt & the rest of the Argo BGC bias adjustment project (Bushinsky, Nachod, Fassbender, Williams)
+#
+
+import numpy as np
+import pandas as pd
+import xarray as xr
+import glob, os
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+from datetime import datetime, date, time
+import time
+from scipy import stats
+import carbon_utils
+
+
+# Function for converting date to year fraction (taken from stack overflow: https://stackoverflow.com/questions/6451655/how-to-convert-python-datetime-dates-to-decimal-float-years)
+
+def toYearFraction(date):
+    def sinceEpoch(date): # returns seconds since epoch
+        return time.mktime(date.timetuple())
+    s = sinceEpoch
+
+    year = date.year
+    startOfThisYear = datetime(year=year, month=1, day=1)
+    startOfNextYear = datetime(year=year+1, month=1, day=1)
+
+    yearElapsed = s(date) - s(startOfThisYear)
+    yearDuration = s(startOfNextYear) - s(startOfThisYear)
+    fraction = yearElapsed/yearDuration
+
+    return date.year + fraction
+
+
+# +
+# read in a user-created text file to point to local directories to avoid having to change this every time 
+# we update code
+lines=[]
+with open('path_file.txt') as f:
+    lines = f.readlines()
+    
+count = 0
+for line in lines:
+    count += 1
+    index = line.find("=")
+    #print(f'line {count}: {line}')
+    #print(index)
+    #print(line[0:index])
+    line = line.rstrip()
+    if line[0:index].find("argo")>=0:
+        argo_path=line[index+1:]
+    elif line[0:index].find("liar")>=0:
+        liar_dir=line[index+1:]
+    elif line[0:index].find("matlab")>=0:
+        matlab_dir=line[index+1:]
+        
+# Set the paths
+output_dir = 'output/'
+data_dir = 'data/'
+
+
+# +
+output_dir_figs = 'figures_o2_bias/'
+
+#check directories exist
+if not os.path.isdir('figures_o2_bias'):
+    os.mkdir('figures_o2_bias')
+# -
+
+# 1. Read in all float Sprof files from argo path
+
+# +
+argolist = []
+for file in os.listdir(argo_path):
+    if file.endswith('Sprof.nc'):
+        argolist.append(file)
+
+last = 0
+# -
+
+# 2. For each file, find all profiles with valid O2 data, save out lat/lon/date into an array
+
+#wmo_list = list()
+for n in range(last, len(argolist)):
+    print(f' {n}' ' File: ' + argolist[n]) 
+
+    #n = 0
+    argo_n = xr.load_dataset(argo_path+argolist[n])
+    argo_n = argo_n.set_coords(('PRES_ADJUSTED','LATITUDE','LONGITUDE','JULD'))
+    
+    var_list = list(argo_n.data_vars)
+    if "DOXY_ADJUSTED" not in var_list:
+        print('Skipping')
+        continue
+            
+    doxy_trimmed = argo_n.DOXY_ADJUSTED.where(~np.isnan(argo_n.DOXY_ADJUSTED), drop=True)
+    wmo_n = argo_n.PLATFORM_NUMBER.values.astype(int)[0]
+
+    prof_loc = xr.Dataset()
+    prof_loc['wmo']=(['N_PROF'],np.repeat(wmo_n,len(doxy_trimmed)))
+
+    prof_loc['LATITUDE'] = (['N_PROF'], doxy_trimmed.LATITUDE.data)
+    prof_loc['LONGITUDE'] = (['N_PROF'], doxy_trimmed.LONGITUDE.data)
+    prof_loc['juld'] = (['N_PROF'],doxy_trimmed.JULD.data)
+    #prof_loc['profile'] = (['N_PROF'],doxy_trimmed.CYCLE_NUMBER.data)
+    #argo_n
+    # append all files into one long xarray
+    if n == 0:
+        argo_all = prof_loc
+    else:
+        argo_all = xr.concat([argo_all,prof_loc], 'N_PROF')
+
+    last = n + 1
+argo_all
+
+# Plotting
+
+# plotting all data
+ax = plt.axes(projection=ccrs.PlateCarree())
+ax.coastlines()
+ax.set_global()
+plt.plot(argo_all.LONGITUDE, argo_all.LATITUDE, linestyle='none', marker='.', markersize=1)
+plt.show()
+
+# +
+# Average data by wmo. Longitude is currently incorrectly averaged, which is fixed below
+argo_wmo = argo_all.groupby('wmo').mean()
+
+avg_date = argo_all.juld.astype('int64').groupby(argo_all['wmo']).mean().astype('datetime64[ns]')
+
+# calculate the year fraction for plotting
+avg_date_dec_year = []
+for i in avg_date.values:
+    x = pd.Timestamp(i)
+    y = toYearFraction(x)
+    avg_date_dec_year.append(y)
+# -
+
+
+for wmo in argo_wmo.wmo:
+    temp_LONGITUDE = argo_all.LONGITUDE.where(argo_all.wmo==wmo, drop=True)
+    if (np.max(temp_LONGITUDE) - np.min(temp_LONGITUDE))>300:
+        print('YES')
+        new_mean = np.mean(xr.where(temp_LONGITUDE>=0, temp_LONGITUDE, temp_LONGITUDE+360))
+        if new_mean>180:
+            new_mean = new_mean-360
+            old_mean = argo_wmo.LONGITUDE.where(argo_wmo.wmo==wmo, drop=True)
+
+        #print('old mean: '  + str(old_mean.values))
+        #print('new mean: '  + str(new_mean.values))
+
+        # put new LONGITUDE mean back into argo_wmo
+        argo_wmo['LONGITUDE'] = argo_wmo.LONGITUDE.where(argo_wmo.wmo!=wmo, new_mean)
+        #updated_mean = argo_wmo.LONGITUDE.where(argo_wmo.wmo==wmo, drop=True)
+
+        #print('new mean: '  + str(updated_mean.values))
+
+        #new_mean
+    #else:
+        #print('NO')
+
+# +
+# Figure 1
+
+fig = plt.figure(figsize=(20,12))
+ax = plt.axes(projection=ccrs.PlateCarree())
+ax.coastlines()
+ax.set_global()
+sct = plt.scatter(x=argo_wmo.LONGITUDE, 
+            y=argo_wmo.LATITUDE, 
+            c=avg_date_dec_year,cmap='turbo',s=10)
+cbar = plt.colorbar(sct, fraction=.08, pad = 0.04, shrink=0.5)
+cbar.set_label('Year', labelpad=15, fontsize=14)
+plt.savefig(output_dir_figs+ 'Fig_1_O2_float_map_date.png')
+plt.show()
+
+# -
+
+# Alternate version of Figure 1: gridded observational density
+
+# +
+# Gridded density plot taken from J. Lilly's Distributional-Analysis code: http://www.jmlilly.net/course/labs/html/DistributionalAnalysis-Python.html
+figsize=np.array([18, 12]);
+projection=ccrs.PlateCarree();
+
+cmap = plt.cm.get_cmap("Spectral_r", 64)
+
+#for future reference, define a function to set up our map
+def map_setup(figsize,projection):
+    fig = plt.figure(figsize=figsize)
+    ax = fig.add_subplot(projection=projection)
+    ax.add_feature(cfeature.LAND, facecolor='grey')
+    gl = ax.gridlines(draw_labels=True)  
+    gl
+    gl.xlabel_style = {'size': 15}
+    gl.ylabel_style = {'size': 15}
+    return fig, ax 
+
+fig, ax = map_setup(figsize,projection) 
+
+dlatlon = 1
+lonbins = np.arange(-180, 180, dlatlon)
+latbins = np.arange(-80, 80, dlatlon)
+
+hist = stats.binned_statistic_2d(argo_all.LATITUDE, argo_all.LONGITUDE, None, bins=[latbins, lonbins], statistic="count")
+hist.statistic[hist.statistic == 0] = np.nan  # Sets all values of 0 to nan as log10(0) = -inf
+hist.statistic
+
+image = plt.pcolormesh(lonbins, latbins, hist.statistic, cmap=cmap, shading="flat", transform=ccrs.PlateCarree()) 
+plt.clim(0, 100) 
+
+cbar = fig.colorbar(image, ax=ax, orientation='horizontal', fraction=0.1, aspect=40, pad=0.08)
+               
+cbar.ax.tick_params(labelsize=20)
+cbar.set_label(label='Number of Observations', size=20)
+plt.savefig(output_dir_figs+ 'Fig_1v2_Argo_O2_Sampling_Density.png')
+# -
+
+# Figure 3. Histograms showing any bias in bgc parameters from crossovers with glodap
+#
+
+glodap_offsets_mean = xr.load_dataset(output_dir+'glodap_offsets_floatmean_withcalibration.nc')
+
+glodap_offsets_p = glodap_offsets_mean.to_dataframe()
+
+parameter_a = 'o2_calib_air_group'
+parameter_b = 'pH_group'
+offsets_g = glodap_offsets_p.groupby(parameter_a)
+
+offsets_pH = glodap_offsets_p.groupby([parameter_a, parameter_b])
+
+n[0]
+
+# +
+
+plt.figure(figsize=(10,8))
+plt.subplot(2,2,1)
+nmean = np.around(glodap_offsets_mean['DOXY_ADJUSTED_offset'].mean(), decimals=2)
+nmedian = np.around(glodap_offsets_mean['DOXY_ADJUSTED_offset'].median(), decimals=2)
+ncount = glodap_offsets_mean['DOXY_ADJUSTED_offset'].count()
+    
+plt.hist(glodap_offsets_mean['DOXY_ADJUSTED_offset'], 
+         bins=np.linspace(-170, 170, 61),label='mean='+str(nmean.values) + ' median='+str(nmedian.values) + 
+         ', n='+str(ncount.values)) # ,label=str(n)
+#print(np.around(glodap_offsets_mean['DOXY_ADJUSTED_offset'].median().values, decimals=2))
+plt.grid()
+plt.title('All float crossovers')
+plt.xlabel(r'DOXY Offset ( $\mu$mol kg$^{-1}$)')
+plt.legend()
+
+plt.subplot(2,2,3)
+plt.title('All floats, $\pm$ 20 $\mu$mol kg$^{-1}$ range')
+plt.hist(glodap_offsets_mean['DOXY_ADJUSTED_offset'], 
+         bins=np.linspace(-20, 20, 61),label='median='+str(nmedian.values) + 
+         ', n='+str(ncount.values)) # ,label=str(n)
+plt.grid()
+plt.xlabel(r'DOXY Offset ( $\mu$mol kg$^{-1}$)')
+
+plt.subplot(2,2,2)
+
+for n, group in offsets_g:
+    print(n)
+    nmean = np.around(group['DOXY_ADJUSTED_offset'].mean(), decimals=2)
+    nmedian = np.around(group['DOXY_ADJUSTED_offset'].median(), decimals=2)
+    ncount = group['DOXY_ADJUSTED_offset'].count()
+
+    plt.hist(group['DOXY_ADJUSTED_offset'], bins=np.linspace(-20, 20, 61),
+             alpha=0.3,label=str(n)+' \nmed.='+str(nmedian) + 
+         ', n='+str(ncount))
+    plt.grid()
+    plt.title('Grouped by air calibration')
+
+    plt.xlabel(r'DOXY Offset ( $\mu$mol kg$^{-1}$)')
+    plt.legend()
+
+plt.subplot(2,2,4)
+
+for n, group in offsets_pH:
+    if n[1] == 'no pH' or n[0] == 'no cal/bad':
+        continue
+    print(n)
+    nmean = np.around(group['DOXY_ADJUSTED_offset'].mean(), decimals=2)
+    nmedian = np.around(group['DOXY_ADJUSTED_offset'].median(), decimals=2)
+    ncount = group['DOXY_ADJUSTED_offset'].count()
+
+    plt.hist(group['DOXY_ADJUSTED_offset'], bins=np.linspace(-20, 20, 61),
+             alpha=0.3,label=str(n)+' \nmed.='+str(nmedian) + 
+         ', n='+str(ncount))
+    plt.title('pH floats only')
+    plt.grid()
+
+    plt.xlabel(r'DOXY Offset ( $\mu$mol kg$^{-1}$)')
+    plt.legend()
+    
+plt.tight_layout()
+
+plt.savefig(output_dir_figs + 'Glodap_offsets_doxy_all.png')
+
+# apparently need to filter out and understand some very large outliers
+
+# +
+print(np.mean(glodap_offsets.DOXY_ADJUSTED_offset))
+print(np.nanmedian(glodap_offsets.DOXY_ADJUSTED_offset))
+
+plt.figure(figsize=(20,12))
+plt.hist(glodap_offsets.DOXY_ADJUSTED_offset, bins=np.linspace(-400, 400, 401))
+plt.xlabel('DOXY Offset')
+plt.savefig(output_dir_figs + 'Glodap_offsets_doxy_plus_minus_400.png')
+
+# -
+
+# Looking at impact of oxygen offsets on pH / pCO2 / Nitrate / DIC
+
+# +
+# load glodap_offsets with cal info:
+
+glodap_offsets = xr.load_dataset(output_dir+'glodap_offsets_withcalibration.nc')
+
+
+# +
+#create meta groups based on calibration groups (air cal, no air cal, no cal)
+g = glodap_offsets.o2_calib_group.copy(deep=True)
+g = g.where(glodap_offsets.o2_calib_group == 'air cal','not air')
+glodap_offsets['o2_calib_air'] = xr.where(glodap_offsets.o2_calib_group=='bad','no cal',g)
+
+glodap_offsets['o2_calib_air_key'] = xr.where(glodap_offsets.o2_calib_air=='air cal',1,0)
+
+glodap_offsets['o2_not_air_key'] = xr.where(glodap_offsets.o2_calib_air=='not air',-1,0)
+glodap_offsets['o2_cal_key'] = glodap_offsets['o2_calib_air_key'] + glodap_offsets['o2_not_air_key']
+
+wmo_means = glodap_offsets.groupby('main_float_wmo').mean(...)
+# -
+
+# new dataset of air_cal floats only
+a = wmo_means.where(wmo_means.o2_cal_key>0.6, drop=True)
+
+# +
+#load saved argo_interp data for ease of finding 1500m values
+argo_interp = xr.open_dataset(data_dir+'argo_interp_temp.nc')
+
+#group by float wmo
+argo_wmo = argo_interp.groupby('wmo')
+
+# +
+# checking for inconsistencies in interpolated vs. derived lengths
+
+pH_count = 0;
+# load float files, use offset information to calculate impact 
+for n in range(2,3): #range(0, len(a.main_float_wmo)):
+    wmo_n = int(a.main_float_wmo[n].values)
+    
+    argo_derived_n = xr.load_dataset(argo_path+ 'derived/' + str(wmo_n) + '_derived.nc')
+
+    print(str(n) + ': ' + str(a.main_float_wmo[n].values))
+
+    print(argo_wmo[wmo_n].PRES_ADJUSTED.shape)
+    print(argo_derived_n.PRES_ADJUSTED.shape)
+    print(' ')
+    
+# -
+
+argo_derived_n.PRES_ADJUSTED[0,:]
+
+argo_wmo[wmo_n].TEMP_ADJUSTED[:,0]
+
+# +
+LIPHR_path = liar_dir
+
+pH_count = 0;
+# load float files, use offset information to calculate impact 
+for n in range(37,38): #range(0, len(a.main_float_wmo)):
+    wmo_n = int(a.main_float_wmo[n].values)
+    
+    argo_derived_n = xr.load_dataset(argo_path+ 'derived/' + str(wmo_n) + '_derived.nc')
+
+    # check if the float also has pH
+    if 'PH_IN_SITU_TOTAL_ADJUSTED' in argo_derived_n.keys() and np.any(~np.isnan(argo_derived_n.PH_IN_SITU_TOTAL_ADJUSTED)):
+        pH_count = pH_count+1
+        print(str(n) + ': ' + str(a.main_float_wmo[n].values))
+
+    else:
+        continue
+    
+    nprof = argo_derived_n.LATITUDE.shape[0]
+
+    #test_pH = np.empty(nprof, dtype=float)
+    
+    # initialize coordinate and measurement arrays
+    Coordinates_all = np.empty([nprof, 3],dtype=float)
+    Coordinates_all[:] = np.nan
+       
+    Measurements_all = np.empty([nprof, 4],dtype=float)
+    Measurements_all[:] = np.nan
+    
+    MeasIDVec = [1, 7, 3, 6]
+    for p in range(nprof):
+        index_1500 = argo_wmo[wmo_n].PRES_ADJUSTED[p,:]==1500
+
+        #argo_wmo[5904659].PSAL_ADJUSTED[0,index_1500]
+        Coordinates_all[p,:] = np.stack((argo_wmo[wmo_n].LONGITUDE[p].values.flatten(), 
+                            argo_wmo[wmo_n].LATITUDE[p].values.flatten(), 
+                            argo_wmo[wmo_n].PRES_ADJUSTED[p,index_1500].values.flatten()),
+                            axis=1)
+        Measurements_all[p,:] = np.stack((argo_wmo[wmo_n].PSAL_ADJUSTED[p,index_1500].values.flatten(), 
+                             argo_wmo[wmo_n].TEMP_ADJUSTED[p,index_1500].values.flatten(), 
+                             argo_wmo[wmo_n].NITRATE_ADJUSTED[p,index_1500].values.flatten(), 
+                             argo_wmo[wmo_n].DOXY_ADJUSTED[p,index_1500].values.flatten()),
+                             axis=1)
+    
+    
+    # calculate LIPHR pH for all profiles
+    test_pH = carbon_utils.LIPHR_matlab(LIPHR_path,
+                                    Coordinates_all.tolist(),
+                                    Measurements_all.tolist(),
+                                    MeasIDVec, 
+                                    OAAdjustTF = False)  
+       
+    Measurements_o2_offset = np.concatenate(([Measurements_all[:,0]], 
+                                  [Measurements_all[:,1]],
+                                  [Measurements_all[:,2]],
+                                  [Measurements_all[:,3]] + a.DOXY_ADJUSTED_offset[n].values))
+    Measurements_o2_offset = np.transpose(Measurements_o2_offset)
+    # calculate LIPHR pH for all profiles
+    new_pH = carbon_utils.LIPHR_matlab(LIPHR_path,
+                                    Coordinates_all.tolist(),
+                                    Measurements_o2_offset.tolist(),
+                                    MeasIDVec, 
+                                    OAAdjustTF = False)  
+    argo_derived_n['pH_orig_LIPHR'] = (['N_PROF'],np.empty(argo_derived_n.PRES_ADJUSTED.shape[0])) #nprof x nlevel
+    argo_derived_n.pH_orig_LIPHR[:] = test_pH[:,0]
+
+    argo_derived_n['pH_O2_ADJUST_LIPHR'] = (['N_PROF'],np.empty(argo_derived_n.PRES_ADJUSTED.shape[0])) #nprof x nlevel
+    argo_derived_n.pH_O2_ADJUST_LIPHR[:] = new_pH[:,0]
+    # difference between pH_O2_orig_LIPHR and PH_IN_SITU_TOTAL_ADJUSTED- should that average 0? 
+    # Impact of O2 can be seen in the test_pH minus new_pH average 
+    
+    argo_derived_n['PH_IN_SITU_TOTAL_ADJUSTED_w_O2_ADJUST_OFFSET'] = \
+        argo_derived_n.PH_IN_SITU_TOTAL_ADJUSTED + np.mean(new_pH - test_pH)
+    
+    argo_derived_n.to_netcdf(argo_path+ 'derived/' + str(wmo_n) + '_derived_2.nc')
+
+# -
+
+argo_wmo[5904659]
+
+argo_derived_n
+
+        #call CO2sys to calculate pCO2 with bias correction and no O2 correction (check against original)
+        results = pyco2.sys(
+                par1=argo_derived_n.TALK_LIAR, 
+                par2=argo_derived_n.pH_insitu_corr,
+                par1_type=1,
+                par2_type=3,
+                temperature=argo_n.TEMP_ADJUSTED, 
+                pressure=argo_n.PRES_ADJUSTED, 
+                salinity=argo_n.PSAL_ADJUSTED, 
+                temperature_out=25.* np.ones(argo_n.PRES_ADJUSTED.shape), #fixed 25C temperature
+                pressure_out=argo_n.PRES_ADJUSTED,
+                total_silicate=SI,
+                total_phosphate=PO4,
+                opt_pH_scale = 1, #total
+                opt_k_carbonic=10, #Lueker et al. 2000
+                opt_k_bisulfate=1, # Dickson 1990 (Note, matlab co2sys combines KSO4 with TB. option 3 = KSO4 of Dickson & TB of Lee 2010)
+                opt_total_borate=2, # Lee et al. 2010
+                opt_k_fluoride=2, # Perez and Fraga 1987
+                opt_buffers_mode=1,
+                )
+
+
+test_pH - new_pH
+plt.hist(new_pH - test_pH)
+np.mean(new_pH - test_pH)
+
+plt.hist(argo_wmo[wmo_n].PH_IN_SITU_TOTAL_ADJUSTED[:,index_1500] - test_pH)
+np.mean(argo_wmo[wmo_n].PH_IN_SITU_TOTAL_ADJUSTED[:,index_1500] - test_pH)
